c3ea3f6321437011f554667cc804d53a6312b5d5
redhatlinux10 <bruce.oy@gmail.com>
2015-03-12 01:08:15 +0800

    合并查找逻辑，添加对 Chrome 42 的支持

--- AutoBlockScreenLocker.sh
+++ AutoBlockScreenLocker.sh
@@ -2,16 +2,11 @@
 
 
 while true; do
-    pids_of_libpepflashplayer=($(ps aux|grep libpepflashplayer.so|grep -v "grep" | awk '{print $2}'))
-    pids_of_firefox_plugin_container=($(ps aux|grep 'firefox/plugin-container'|grep -v "grep" | awk '{print $2}'))
-    if test ${#pids_of_libpepflashplayer[@]} -ne 0
+    pids_of_flashplayer_process=($(ps aux|grep "libpepflashplayer.so\|-type=ppapi\|firefox/plugin-container"|grep -v "grep" | awk '{print $2}'))
+    if test ${#pids_of_flashplayer_process[@]} -ne 0
     then
-        echo "libpepflashplayer processes detected. try to prevent screen locker from running."
+        echo "flashplayer processes detected. try to prevent screen locker from running."
         qdbus org.freedesktop.ScreenSaver /ScreenSaver SimulateUserActivity > /dev/null
-    elif test ${#pids_of_firefox_plugin_container[@]} -ne 0
-    then
-      echo "firefox plugin container process detected. try to prevent screen locker from running."
-      qdbus org.freedesktop.ScreenSaver /ScreenSaver SimulateUserActivity > /dev/null
     fi
     sleep 30
 done
