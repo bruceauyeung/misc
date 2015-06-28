44c8b2d299618a2166eeacb5a997b3457db8e41d
redhatlinux10 <bruce.oy@gmail.com>
2015-06-28 15:21:03 +0800

    add comments

--- smart-zypper-update.sh
+++ smart-zypper-update.sh
@@ -21,24 +21,40 @@ echo 'this script will call "zypper update" with additional features as follows:
 (3) automatically trust and import new repository signing keys.
 (4) automatically say "yes" to third party license confirmation prompt.
 '
+# index of repository. used to disable repository
 repo_idx=0
+# store all unreachable repositories. only for display purpose.
 unreachable_repos=()
-update_options_str=""
+
+update_options_str="-l"
+
+# -2 是为了排除列标题和紧接着的空行
 repo_count=`zypper ls -u | awk -F'|' 'END{  print NR - 2}'`
 OLD_IFS=$IFS
+
+# 由于 repo name 可能包含空格，所以必须要求 for in 只按换行符来识别数组元素
 IFS=$'\n' 
 for name_url in `zypper ls -u | awk -F'|' '{ if (NR > 2) print $3"|"$8}'`;do
+
+    # 数值相加
     repo_idx=$((repo_idx + 1))
     
+    # 按 | 来分割名称和URL
     repo_name=`echo $name_url|awk -F'|' '{print $1}'`
     repo_url=`echo $name_url|awk -F'|' '{print $2}'`
     
     repo_url=$(str_trim "$repo_url")
+    
+    # bash 方式的 startsWith
     if [[ $repo_url == http* ]]; then
+    
+       # 访问文件并获取 HTTP STATUS CODE
        url_status=`curl --connect-timeout 10 -Is $repo_url/repodata/repomd.xml | head -n 1 |awk '{print $2}'`
        
        if [ $url_status -eq 200 ] ; then
            echo '('$repo_idx'/'$repo_count')' "$repo_name Access OK" 
+           
+           # 组装 update_options_str。包括只从可达的 repo 更新软件
            update_options_str="$update_options_str -r $repo_idx"
        else 
            echo '('$repo_idx'/'$repo_count')' "$repo_name Access Failed" 
@@ -53,6 +69,6 @@ do
 done
 echo "temporarily disable these repositories and start to update."
 IFS=$OLD_IFS 
-sudo zypper -n --gpg-auto-import-keys up -l $update_options_str
+sudo zypper -n --gpg-auto-import-keys up $update_options_str
 
 
